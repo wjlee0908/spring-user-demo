7a4abbe1ccdf920084022260eaa1d07eee4f62ae
Woojin Lee <dev.woojin.lee@gmail.com>
2024-03-20 20:24:32 +0900

    Initial commit

---
+++ src/main/java/com/woojin/userdemo/SpringUserDemoApplication.java
@@ -0,0 +1,13 @@
+package com.woojin.userdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woojin/userdemo/SpringUserDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojin.userdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
