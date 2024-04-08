b48a25a2d47bd18e1dd2cb576d81464752f2426c
Woojin Lee <dev.woojin.lee@gmail.com>
2024-04-09 00:59:23 +0900

    feature/BT-39/monitoring (#4)
    * [feature] (actuator) add actuator and info
    
    * [feature] (actuator) add health properties
    
    * [docs] (actuator) add httptrace comment
    
    * [feature] (actuator) add user count to /info
    
    * [feature] (actuator) add git commit info to /info
    
    * [feature] (actuator) add custom health indicator
    
    * [feature] (prometheus) add prometheus
    
    * [feature] (README) add monitoring modules

---
+++ src/main/java/com/woojin/userdemo/actuator/CustomHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.woojin.userdemo.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring Actuator의 /health에 응답할 정보
+ */
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        int hour = 1;
+
+        // 외부 서비스를 사용 가능한 지 확인한다
+
+//        return Health.outOfService().withDetail("reason", "External service is out of service")
+//                .withDetail("hour", hour)
+//                .build();
+        return Health.up().build();
+    }
+}
